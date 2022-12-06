90bfd561015cf31ea418b34be5dfab9e21468d4f
Eliasi1 <eliasiohad@gmail.com>
2022-12-06 08:36:09 +0200

    first commit

---
+++ js/controller.js
@@ -0,0 +1,69 @@
+'use strict';
+
+function onInit() {
+    addBook('harry potter 1', 'Jk.Rowling')
+    addBook('harry potter 2', 'Jk.Rowling')
+    addBook('harry potter 3', 'Jk.Rowling')
+    addBook('the lost simbol', 'Dan Bar-on')
+    addBook('tzofen de vinchi', 'Dan Bar-on')
+    addBook('the source', 'Dan Bar-on')
+    addBook('loonar storm', 'Ohad Elias')
+    addBook('irretional man', 'Ohad Elias')
+    renderBooks()
+}
+
+function renderBooks() {
+    console.log("Rendering books..")
+    var strHTML = CreateBooksHTMLStr()
+    document.querySelector(".book-list").innerHTML = strHTML
+}
+
+
+function renderFilterOptions() {
+    var strHTML = CreateFilterOptionsHTMLStr()
+    document.querySelector(".filter-writer").innerHTML = strHTML
+}
+
+function onBookDelete(id) {
+    deleteBook(id)
+    renderBooks()
+}
+
+function onBookUpdate(name, id, currPrice) {
+    var newPrice = +prompt(`set new price for: ${name}`, currPrice)
+    updateBook(id, newPrice)
+    renderBooks()
+}
+
+function onCreateBook() {
+    var name = prompt("what is the book name?")
+    var writer = prompt("who is the book's writer?")
+    addBook(name, writer)
+    renderBooks()
+}
+
+function onBookBrief(id) {
+    var currBook = getBookById(id)
+    var currEl = document.querySelector('.brief-modal')
+    currEl.classList.add("open")
+    currEl.querySelector('h3').innerText = `${currBook.name} - ${currBook.writer}`
+    currEl.querySelector('h4 span').innerText = `${currBook.price}$`
+    currEl.querySelector('p').innerText = currBook.desc
+
+}
+
+function onCloseModal() {
+    var currEl = document.querySelector('.brief-modal')
+    currEl.classList.remove("open")
+}
+
+function onSetFilterBy(writer) {
+    setFilterBy(writer)
+    renderBooks()
+
+}
+
+function onSelectSortBy(value) {
+    onSelectSortBy(value)
+    renderBooks()
+}
---
+++ js/services/book.service.js
@@ -0,0 +1,116 @@
+'use strict';
+
+var gBooks = []
+var gWriters = []
+var STORAGE_KEY = 'bookDB'
+var gFilterBy = {writer : '', maxPrice : 300}
+var gPage = 1
+
+
+function getBooks(){
+    var filtereByWriter = gBooks.filter((book) => !gFilterBy.writer ? book : gFilterBy.writer === book.writer)
+    var filteredByPrice = filtereByWriter.filter((book) => book.price < gFilterBy.maxPrice)
+    return filteredByPrice
+}
+
+function CreateBooksHTMLStr(){
+    var strHTML = getBooks().map(book => `
+    <tr class="book-row">
+    <td><img src="img/${book.name}.jpg" width="100px" height="150px" onerror='this.onerror=null;this.src="img/default.jfif"'></td>
+    <td>
+        <h5 class="name">Name: <span>${book.name}</span></h5>
+        <h5 class="writer">Writer: <span>${book.writer}</span></h5>
+        <h5 class="price">price: <span>${book.price}</span>$</h5>
+        <h5 class="book-id">book-id: <span>${book.id}</span></h5>
+    </td>
+    <td>
+        <button onclick="onBookBrief('${book.id}')">Brief</button>
+        <button onclick="onBookDelete('${book.id}')">delete</button>
+        <button onclick="onBookUpdate('${book.name}','${book.id}','${book.price}')">update</button>
+    </td>
+</tr>
+    `)
+    return strHTML.join('')
+}
+
+function CreateFilterOptionsHTMLStr(){
+    var strHTML = gWriters.map((writer) =>`
+    <option value="${writer}">${writer}</option>
+    `)
+    strHTML.push('<option value="">All</option>')
+    return strHTML.join('')
+}
+
+function _createBooks(){}
+
+function _createBook(name, writer) {
+   return {
+    id : makeId(),
+    name,
+    writer,
+    price : getRandomIntInclusive(50,250),
+    desc : makeLorem()
+    // imgUrl : `img/${name}`
+   }
+}
+
+function addBook (name,writer){
+    const book = _createBook(name,writer)
+    gBooks.unshift(book)
+    updateWritersList()
+    // _saveBooksToStorage()
+    return book
+}
+
+function updateWritersList(){
+    gWriters = gBooks.reduce((writersArr,book) => {
+        console.log("Called with ", writersArr, book.name)
+        var currBookWriter = book.writer
+        if (!writersArr.includes(currBookWriter))  writersArr.push(currBookWriter)
+        return writersArr
+    },["select-writer"])
+    renderFilterOptions()
+}
+
+function _saveBooksToStorage(){
+    saveToStorage('gBooks', gBooks)
+}
+
+function deleteBook(bookId){
+    gBooks = gBooks.filter((book)=>book.id !== bookId)
+}
+
+function updateBook(bookId, newPrice){
+    var requestedIdx = gBooks.findIndex((book)=>book.id === bookId)
+    gBooks[requestedIdx].price = newPrice
+}
+
+function getBookById (bookId){
+    return gBooks.find((book)=> book.id === bookId)
+}
+
+function setFilterBy(writer){
+    if (writer.writer) gFilterBy.writer = writer.writer
+    if (writer.maxPrice) gFilterBy.maxPrice = writer.maxPrice
+
+}
+
+function onSelectSortBy(value){
+    switch (value){
+        case 'price':
+            gBooks.sort((a,b) => a.price - b.price)
+            break
+        case 'writer':
+            gBooks.sort((a,b) => {
+                const nameA = a.writer.toUpperCase()
+                const nameB = b.writer.toUpperCase()
+                if (nameA>nameB) return 1
+                else if (nameA<nameB) return -1
+                else return 0     
+            })
+            break
+        case '':
+            break
+    }
+}
+
